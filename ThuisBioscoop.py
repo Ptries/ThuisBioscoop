3b31031024673f5b0dc091c986d3d28a77879b29
Patrice Diekhorst <patrice.diekhorst@student.hu.nl>
2016-11-08 14:49:10 +0100

    Aanpassing Bestand
    Tkinter aangemaakt

--- ThuisBioscoop.py
+++ ThuisBioscoop.py
@@ -1,3 +1,7 @@
-#Start van het project Thuisbioscoop
-#Projectgroep 3 klas V1M
+"""
+Start van het project Thuisbioscoop
+Projectgroep 3 klas V1M
+"""
 
+from tkinter import Tk, RIGHT, BOTH, RAISED
+from tkinter.ttk import Frame, Button, Style
