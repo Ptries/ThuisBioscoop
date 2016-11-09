c2611ec7e67a1333624b6b53b3f6272047c243c6
Patrice Diekhorst <patrice.diekhorst@student.hu.nl>
2016-11-09 10:25:49 +0100

    Dictreader met API geïmplementeerd

--- ThuisBioscoop.py
+++ ThuisBioscoop.py
@@ -3,5 +3,18 @@ Start van het project Thuisbioscoop
 Projectgroep 3 klas V1M
 """
 
-from tkinter import Tk, RIGHT, BOTH, RAISED
-from tkinter.ttk import Frame, Button, Style
+import time
+import urllib.request
+import xmltodict
+
+def openAPI():
+    datumVandaag = time.strftime("%d-%m-%Y")
+    APIlink = 'http://api.filmtotaal.nl/filmsoptv.xml?apikey=zky5pqtra4entxe7lk3agqqtk8snjgry&dag=' + str(
+        datumVandaag) + '&sorteer=0'
+    response = urllib.request.urlopen(APIlink).read()
+    xmldictionary = xmltodict.parse(response)
+    return xmldictionary
+
+filmsVandaag = openAPI()
+
+print(filmsVandaag)
